d012170781178bc3285dd20536928a0a596447d1
Olga_ <olga_art1025@mail.ru>
2022-06-18 01:25:11 +0500

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> users = List.of("Olga", "Anna", "Masha", "Zhenya");
+        model.addAttribute("user", users);
         return "index";
     }
 }
